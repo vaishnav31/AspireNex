97c62ba32adbcaa95fb361b92a2c65db64bc3799
vaishnav31 <149086744+vaishnav31@users.noreply.github.com>
2024-07-02 23:35:57 +0530

    Add files via upload

---
+++ TASK1.py
@@ -0,0 +1,28 @@
+#chatbot making
+
+name=input("Enter Your Name:")
+print("Hello",name,"!")
+
+for i in range (10000):
+    chat=str(input("Ask Some Question?  hoe"))
+
+    if chat=="what is your name":
+       print("My Name is Alex")
+
+    if chat=="what is your purpose":
+       print("my purpose to assist and provide information")
+
+    if chat=="who made you":
+       print("Vaishnav Made Me")
+
+    if chat=="how do you spell hello":
+       print("H-E-L-L-O")
+
+    if chat=="what is largest planet in our solar system":
+       print("largest planet is jupiter")
+    
+    if chat=="how many days in week":
+       print("there arer 7 days in week")
+
+    if chat=="what is your favorite food":
+       print("Your data")
